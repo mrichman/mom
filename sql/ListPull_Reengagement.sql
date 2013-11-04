fe22ee04bdfbb50f55c0c2d44cf479996eb75034
Mark Richman <mark@markrichman.com>
2013-11-04 14:48:15 -0500

    Updated with CTE to exclude recently ordering customers.

--- sql/ListPull_Reengagement.sql
+++ sql/ListPull_Reengagement.sql
@@ -1,18 +1,29 @@
+USE [Mom-Nutri-Health]
+GO
+/****** Object:  StoredProcedure [dbo].[ListPull_Reengagement]    Script Date: 11/04/2013 11:11:37 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 -- =============================================
 -- Author:		Mark Richman
 -- Create date: 10/8/2013
 -- Description:	Re-engagement File
 -- Example: exec ListPull_Reengagement
 -- =============================================
-ALTER PROCEDURE [dbo].ListPull_Reengagement
+ALTER PROCEDURE [dbo].[ListPull_Reengagement]
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	DECLARE @120daysago DATETIME = DATEADD(DAY, -120, DATEDIFF(DAY,0,GETDATE()))
-	PRINT @120daysago
-    SELECT
+	
+	;WITH CTE_LastOrder(CUSTNUM, ODR_DATE) AS
+	(
+		SELECT o.CUSTNUM, MAX(o.ODR_DATE) ODR_DATE
+		FROM CMS o
+		GROUP By o.CUSTNUM
+	)
+	
+    SELECT 
 		LTRIM(RTRIM(EMAIL)) AS EMAIL,
 		c.CUSTNUM,
 		LTRIM(RTRIM(FIRSTNAME)) AS FIRSTNAME,
@@ -20,13 +31,15 @@ BEGIN
 		MAX(o.ODR_DATE) AS ODR_DATE
 	FROM CUST c
 	INNER JOIN CMS o ON (c.CUSTNUM = o.CUSTNUM)
+	JOIN CTE_LastOrder LO ON (c.CUSTNUM = LO.CUSTNUM)
 	LEFT OUTER JOIN ClubView cv ON(c.CUSTNUM = cv.BILL_CUST)
 	WHERE c.NOEMAIL = 0
 		AND c.EMAIL IS NOT NULL
 		AND c.EMAIL <> ''
 		AND c.EMAIL NOT LIKE '%@amazon.com'
 		AND cv.Stat <> 'A' -- exclude autoships
-		AND o.ODR_DATE <= @120daysago
+		AND LO.ODR_DATE > DATEADD(MONTH, -3, CURRENT_TIMESTAMP)
 	GROUP BY EMAIL, c.CUSTNUM, FIRSTNAME, LASTNAME
 	ORDER BY MAX(o.ODR_DATE) DESC
+	
 END
